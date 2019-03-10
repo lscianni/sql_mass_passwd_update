461b63a30beea6aefc426d98b687fbcd9218c7d6
lscianni <lscianniit@gmail.com>
2019-03-10 19:37:54 -0400

    commit file

--- sql_mass_pass_update.py
+++ sql_mass_pass_update.py
@@ -3,7 +3,7 @@
 # 2018-02-13
 """
 ------------------------------------------------------------------------------
-Copyright (C) <year>  <name of author>
+Copyright (C) 2019  Louis Scianni
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
